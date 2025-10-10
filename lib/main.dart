da99a45f1b9216fe81fb331a3657252845ce360e
Joshua Alcazar <alcajosh@outlook.com>
2025-10-10 10:36:21 +0100

    Change text

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('Test commit:'),
+            const Text('I love pressing buttons:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
